88b67ec2f0b6586db8b7edee001730ec5a46451e
lyl <254364502@qq.com>
2020-07-02 10:50:14 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # yii2-admin-backup
 a tools for yii2-admin role back up
+
+### add controller in yii2 console command
+```php
+'controllerMap' => [
+    'rbac' => 'easydowork\rbacBackup\RbacController',
+],
+or
+'controllerMap' => [
+    'rbac' => [
+        'class' => 'easydowork\rbacBackup\RbacController',
+        'cachePath' => '@console/rbacBackUpCache',
+        'roleCachePath' => '@backend/runtime/cache',
+    ],
+],
+```
+
+```shell script
+php yii rbac/back-up
+php yii rbac/update
+```
---
+++ src/RbacController.php
@@ -0,0 +1,110 @@
+<?php
+namespace easydowork\rbacBackup;
+
+use mdm\admin\components\Configs;
+use Yii;
+use yii\caching\TagDependency;
+use yii\console\Controller;
+use yii\db\Query;
+use yii\helpers\Console;
+use yii\helpers\FileHelper;
+
+/**
+ * Class RbacController
+ * @property string $cachePath
+ * @property string $roleCachePath
+ * @property array $tableList
+ * @package console\controllers
+ */
+class RbacController extends Controller
+{
+
+    /**
+     * @var string
+     */
+    public $cachePath = '@console/rbacBackUpCache';
+
+
+    /**
+     * @var string
+     */
+    public $roleCachePath = '@backend/runtime/cache';
+
+    /**
+     * @var array
+     */
+    public $tableList = [
+        'auth_rule',
+        'auth_item',
+        'auth_item_child',
+        'menu',
+    ];
+
+    /**
+     * init
+     */
+    public function init()
+    {
+        $this->validationDir();
+    }
+
+    public function validationDir()
+    {
+        $dir = Yii::getAlias($this->cachePath);
+
+        try {
+            FileHelper::createDirectory($dir,0777);
+            if (!@chmod($dir, 0777)) {
+                $this->stdout("Operation chmod not permitted for directory $dir.",Console::BG_RED);
+            }
+        }catch (\Exception $e){
+            $this->stdout($e->getMessage(),Console::BG_RED);
+        }
+    }
+
+    /**
+     * actionBackUp
+     */
+    public function actionBackUp()
+    {
+        try {
+            foreach ($this->tableList as $table){
+                $fileName = Yii::getAlias($this->cachePath).DIRECTORY_SEPARATOR.$table;
+                file_put_contents($fileName,serialize((new Query())->from($table)->all()));
+            }
+            $this->stdout('back up ok.',Console::BG_GREEN);
+        }catch (\Exception $e){
+            $this->stdout($e->getMessage(),Console::BG_RED);
+        }
+
+    }
+
+    /**
+     * actionUpdate
+     */
+    public function actionUpdate()
+    {
+        try {
+            $db = Yii::$app->db;
+            foreach ($this->tableList as $table){
+                $fileName = Yii::getAlias($this->cachePath).DIRECTORY_SEPARATOR.$table;
+                $data = unserialize(file_get_contents($fileName));
+                if(!empty($data)){
+                    $db->transaction(function () use ($db,$table,$data){
+                        $db->createCommand()->truncateTable($table);
+                        $db->createCommand()->batchInsert($table,array_keys($data[0]),$data)->execute();
+                    });
+                }
+            }
+            //invalidate rbac cache
+            Yii::$app->cache->cachePath = Yii::getAlias($this->roleCachePath);
+
+            TagDependency::invalidate(Yii::$app->cache, Configs::CACHE_TAG);
+
+            $this->stdout('update ok.',Console::BG_GREEN);
+        }catch (\Throwable $e){
+            $this->stdout($e->getMessage(),Console::BG_RED);
+        }
+    }
+
+}
